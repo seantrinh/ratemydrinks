da8a3fb56c9b0ae18233488997d4e01a01a7726f
Sean Trinh <strinh@stevens.edu>
2019-05-08 15:45:52 -0400

    moved search to beverage

--- data/beverage.js
+++ data/beverage.js
@@ -8,5 +8,15 @@ modules.export = {
 	},
 	async updateRating(name, newRating) {
 		//update rating and increment count
-	}  
+	},
+	async search(list_of_params) {
+                //given a list of search params such as
+                //      min_num_stars
+                //      type of drink
+                //      name of drink
+                //      etc.
+                //find a list of length x of drinks that
+                //match those search results
+                // return beverage name
+        }  
 }
