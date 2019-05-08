b9ea56f4dd7271cf024c9ffc3c68280be10941c7
Sean Trinh <strinh@stevens.edu>
2019-05-08 15:48:08 -0400

    update index

--- data/index.js
+++ data/index.js
@@ -2,13 +2,11 @@
 const account = require("./account");
 const comments = require("./comments");
 const posts = require("./posts");
-const search = require("./search");
 const beverage = require("./beverage");
  
 module.exports = {
 	account: account,
 	comments: comments,
 	posts: posts,
-	search: search,
 	beverage: beverage
 }
