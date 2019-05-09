a800d319ff6e3932ab6478b31ad7f84e35da4d63
Hariharan Vijayachandran <hariv@Hariharans-MacBook-Pro.local>
2019-05-08 20:11:25 -0400

    app.js and routes setup, tb completed

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// Rate my drinks
+const express = require("express");
+const app = express();
+const configRoutes = require("./routes");
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(session({
+  name: 'ratemydrinks',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: true
+}))
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,21 @@
-function constructorMethod(app) {
 
+const index = app=>{
+	function logging (req,res,next){
+		let usr = null;
+		if(req.session.user){
+			usr = "(Authenticated User)";
+		}else{
+			usr = "(Non-Authenticated User)";
+		}
+		
+		console.log("["+(new Date().toUTCString())+"] "+ req.method+" "+req.originalUrl+" "+usr);
+		next();
+	}
+	app.use(logging);
+	app.get("*",(req,res)=>{
+		res.status(404).send("Page not Found");;
+	});
 }
 
-module.exports = constructorMethod;
+
+module.exports = index;
