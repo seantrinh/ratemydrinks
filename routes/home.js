808f1c309802a66d0c42211c8eeb11a047358e1d
Hariharan Vijayachandran <hariv@Hariharans-MacBook-Pro.local>
2019-05-11 17:01:19 -0400

    search done

--- routes/home.js
+++ routes/home.js
@@ -11,22 +11,32 @@ router.post("/", async (req, res) => {
 	//	send res the items
 	if(req.session.user){
 		try {
-			let result = await beverage.search(req.body);
-			res.render("layouts/search", { title: "Drinks Found", drinks: result });
+			let body = {
+                                type: req.body.type,
+                                subtype: req.body.subtype,
+                                tastes: req.body.tastes.split(" "),
+                                name: req.body.beverage_name,
+                                company: req.body.company,
+                                rating: parseFloat(req.body.rating)
+
+                        };
+			let result = await beverage.search(body);
+
+			res.render("layouts/search", { heading: "Drinks Found", drinks: result, USERNAME: req.session.user, LOGOUT_LINK: "Logout"});
 		} catch (e) {
-			res.status(400).render('layouts/error', { title: "400 Error" });
+			res.status(400).render('layouts/error', { heading: "400 Error" });
 		}
 	}else{
-		res.redirect("/home_unauth");
+		res.redirect(307,"/home_unauth");
 	}
 
 });
 router.get("/",async(req,res)=>{
 	if(req.session.user){
 		try{
-			res.render("layouts/search", { title: "Drinks Found" });
+			res.render("layouts/search", { heading: "Drinks Found",drinks: 5, USERNAME: req.session.user, LOGOUT_LINK: "Logout" });
         } catch (e) {
-                res.status(400).render('layouts/error', { title: "400 Error" });
+                res.status(400).render('layouts/error', { heading: "400 Error" });
         }
 	} else{
 		res.redirect("/home_unauth");
