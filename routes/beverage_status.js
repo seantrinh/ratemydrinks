7450de040c1ff7fe6c81ee45ef0048c637a507b0
Sean Trinh <strinh@stevens.edu>
2019-05-12 14:14:43 -0400

    create beverage stuff. still a bug

---
+++ routes/beverage_status.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const data = require("../data");
+const beverage = data.beverage;
+const path = require('path');
+const fs = require('fs').promises;
+
+router.post("/", async(req,res) => {
+    // Will need to reroute to home page
+    if ( !req.session.user) {
+        res.redirect("/");
+    }
+    else {
+	let type = req.body.type;
+	let subtype = req.body.subtype;
+	let tastes = req.body.tastes;
+	let name = req.body.name;
+	let company = req.body.company;
+        try {
+		console.log(name);
+		console.log(type);
+		console.log(subtype);
+		console.log(tastes);
+		console.log(company);
+		await beverage.createBeverage(type, subtype, tastes, name, company); //Needs to be an array
+        }
+        catch(e){
+                console.log(e);
+                res.render('layouts/beverage_status', { error: true });
+                return;
+        }
+        res.render('layouts/beverage_status', { name: name, error: false });
+    }
+});
+module.exports = router;
---
+++ routes/post_beverage.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const data = require("../data");
+const posts = data.posts;
+const beverage = data.beverage;
+const fs = require('fs').promises;
+
+router.get("/",async(req,res) => {
+    // Will need to reroute to home page
+    if (!req.session.user){
+        res.redirect("/");
+    }
+    else{
+        res.render('layouts/post_beverage', {title:'Create a Beverage!'});
+    }
+});
+module.exports = router;
